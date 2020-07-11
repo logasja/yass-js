909db3f7cbd6dcbb24b5b5020bc4520702eca26a
Jacob Logas <logasja@gatech.edu>
2020-07-10 21:42:12 -0400

    Update nDCT signature and test for passing

--- index.d.ts
+++ index.d.ts
@@ -2,16 +2,20 @@ export interface RawImageData<T> {
   width: number;
   height: number;
   data: T;
-  dct_coeffs?: {
-    Y: Int32Array[][] | undefined,
-    U: Int32Array[][] | undefined,
-    V: Int32Array[][] | undefined
-  };
-  quant?: {
-    Y: Int32Array[] | undefined,
-    U: Int32Array[] | undefined,
-    V: Int32Array[] | undefined
-  }
+  dct_coeffs: DCTs;
+  quant: QTs;
+}
+
+export interface DCTs {
+  Y: Int32Array[][] | undefined,
+  U: Int32Array[][] | undefined,
+  V: Int32Array[][] | undefined
+}
+
+export interface QTs {
+  Y: Int32Array[] | undefined,
+  U: Int32Array[] | undefined,
+  V: Int32Array[] | undefined  
 }
 
 type BufferRet = RawImageData<Buffer>;
@@ -20,7 +24,7 @@ type UintArrRet = RawImageData<Uint8Array>;
 type ImageData = BufferRet | UintArrRet;
 type BufferLike = Buffer | Uint8Array | ArrayLike<number> | Iterable<number> | ArrayBuffer;
 
-export declare function encode(imgData: RawImageData<BufferLike>, quality?: number, newDCTTable?: any, is_input_yuv?:boolean): BufferRet;
+export declare function encode(imgData: RawImageData<BufferLike>, quality?: number, nDCT?: DCTs, is_input_yuv?:boolean): BufferRet;
 
 export declare function decode(
   jpegData: BufferLike,
