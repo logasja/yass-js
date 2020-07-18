b831dabdad69aef6ae5e9aaee6ef84e5a0ce9e2b
Jacob Logas <logasja@gatech.edu>
2020-07-18 13:18:06 -0400

    Updating to typescript imp

---
+++ src/types.ts
@@ -0,0 +1,44 @@
+export interface RawImageData<T> {
+  width: number;
+  height: number;
+  data: T;
+  DCT?: DCTs;
+  YCbCr?: YCbCr;
+  exif?: any;
+  comments?: any;
+}
+
+export interface DCTs {
+  Y: Int32Array[][];
+  U?: Int32Array[][];
+  V?: Int32Array[][];
+}
+
+export interface YCbCr {
+  Y: Uint8Array[];
+  Cb?: Uint8Array[];
+  Cr?: Uint8Array[];
+}
+
+export interface DecodePrefs {
+  // "undefined" means "Choose whether to transform colors based on the image’s color model."
+  colorTransform?: boolean;
+  useTArray?: boolean;
+  formatAsRGBA?: boolean;
+  tolerantDecoding?: boolean;
+  maxResolutionInMP?: number; // Don't decode more than 100 megapixels
+  maxMemoryUsageInMB?: number; // Don't decode if memory footprint is more than 512MB
+  withYCbCr?: boolean; // Return the YCbCr arrays
+  withDCTs?: boolean; // Return the decoded DCTs
+}
+
+export type BufferRet = RawImageData<Buffer>;
+export type UintArrRet = RawImageData<Uint8Array>;
+
+export type ImageData = BufferRet | UintArrRet;
+export type BufferLike =
+  | Buffer
+  | Uint8Array
+  | ArrayLike<number>
+  | Iterable<number>
+  | ArrayBuffer;
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+// As the name implies, returns a number clamped to unsigned 8 bit
+function clampTo8bit(a: number): number {
+  return a < 0 ? 0 : a > 255 ? 255 : a;
+}
